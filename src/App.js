6071992e394079683787af59953fd3f2ba3fa553
GeraldRyan <gerald.ryan40@protonmail.com>
2021-05-19 23:12:18 -0500

    'ha'

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          be much harder meathead. Just fucking do it!!!!
+          learn react!!!!
         </a>
       </header>
     </div>
